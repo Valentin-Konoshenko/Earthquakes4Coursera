61879617a08edf1e42b8fc513bf658a8234c77e7
Valentin <konoshenkovalent@ukr.net>
2018-06-02 13:18:51 +0300

    Before getting rid of file_read

--- R/Earhtquakes.R
+++ R/Earhtquakes.R
@@ -11,31 +11,26 @@ if (!require(dplyr)) {
 file_read <- function(filename) {
   if(!file.exists(filename))
     stop("file '", filename, "' does not exist")
-  readr::read_tsv(filename)
+  readr::read_delim(filename, delim = "\t")
 }
 
-get_date <- function(data) {
+get_date <- function(Y, M, D) {
   Shift <- -2400
   T_Date0 <- ISOdate(-Shift, 1, 1, 0)
-  data %>%
-    dplyr::mutate(
-      T_Year = data$YEAR - Shift,
-      T_Date = ISOdate(T_Year, data$MONTH, data$DAY, 0),
-      T_Shift = (as.numeric(T_Date) - as.numeric(T_Date0)) / 3600 / 24,
-      Date = as.Date(T_Shift, "0000-01-01")
-    ) %>%
-    dplyr::select(-T_Year, -T_Date, -T_Shift)
-
+  T_Year = Y - Shift
+  T_Date = ISOdate(T_Year, M, D, 0)
+  T_Shift = (as.numeric(T_Date) - as.numeric(T_Date0)) / 3600 / 24
+  as.Date(T_Shift, "0000-01-01")
 }
 
 eq_clean_data <- function(df) {
   df %>%
     dplyr::select(
       "YEAR", "MONTH", "DAY",
-      "LATITUDE", "LONGITUDE", "LOCATION_NAME") %>%
+      "LATITUDE", "LONGITUDE", "LOCATION_NAME", "COUNTRY") %>%
     dplyr::mutate(MONTH = ifelse(is.na(MONTH), 1, MONTH)) %>%
     dplyr::mutate(DAY = ifelse(is.na(DAY), 1, DAY)) %>%
-    get_date() %>%
+    dplyr::mutate(DATE = get_date(Y = YEAR, M = MONTH, D = DAY)) %>%
     dplyr::select(-YEAR, -MONTH, -DAY)
 }
 
