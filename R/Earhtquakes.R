c4f825a3a99db4a8f2847727a37744fd89de0880
Valentin <konoshenkovalent@ukr.net>
2018-06-07 18:03:33 +0300

    works fine in case size is defined expicitly

--- R/Earhtquakes.R
+++ R/Earhtquakes.R
@@ -15,6 +15,10 @@ if (!require(lubridate)) {
   install.packages("lubridate");
   if(!require(lubridate, character.only = TRUE)) stop("Package lubridate not found");
 }
+if (!require(grid)) {
+  install.packages("grid");
+  if(!require(grid, character.only = TRUE)) stop("Package grid not found");
+}
 #####
 file_read <- function(filename) {
   if(!file.exists(filename))
@@ -42,8 +46,9 @@ eq_clean_data <- function(df) {
     dplyr::mutate(
       DATE = eq_get_date(Y = YEAR, M = MONTH, D = DAY),
       LOCATION_NAME = eq_location_clean(LOCATION_NAME),
-      DEATHS = as.numeric(coalesce(DEATHS, 0)),
-      EQ_MAG_MS = as.numeric(ifense(is.na(EQ_MAG_MS), 0, EQ_MAG_MS))) %>%
+      DEATHS = coalesce(as.numeric(DEATHS), NA_real_),
+      EQ_MAG_MS = coalesce(as.numeric(EQ_MAG_MS), NA_real_),
+      COUNTRY = as.factor(COUNTRY)) %>%
     dplyr::select(
       "DATE", "LATITUDE", "LONGITUDE", "LOCATION_NAME", "COUNTRY",
       "DEATHS", "EQ_MAG_MS")
@@ -51,9 +56,81 @@ eq_clean_data <- function(df) {
 
 NOAA <- file_read("inst\\extdata\\results.txt")
 NOAAC <- eq_clean_data(NOAA)
+
 #####
 
+GeomTimeline <-
+  ggplot2::ggproto(
+    "GeomTimeline", ggplot2::Geom,
+    required_aes = c("xmin", "xmax"),
+    default_aes = ggplot2::aes(shape = 19, colour = NA,
+                               alpha = 0.5##, size = 10##, size = 5
+                               ),
+    draw_key = ggplot2::draw_key_point,
+    draw_group =
+      function(data, panel_scales, coord) {
+        xmin <- as.numeric(data[1, "xmin"])
+        xmax <- as.numeric(data[1, "xmax"])
+        str(data)
+        data_filtered <-
+          data[data$x >= xmin & data$x <= xmax & !is.na(data$x), ] %>%
+          dplyr::mutate(size = if_else(is.na(size), 1, size))
+        str(data_filtered)
+        coords <- coord$transform(data_filtered, panel_scales)
+        str(coords)
+       gp <<- grid::gpar(
+            alpha = coords$alpha,
+          col = coords$colour,
+          fontsize = grid::unit(coords$size * 2, "npc")
+            ##1 *
+            ##ifelse(is.na(coords$size), 1, coords$size)
+        )
+        grid::pointsGrob(coords$x, coords$y, pch = coords$shape
+          ,gp = gp
+          )
+      }
+    )
+#####
+geom_timeline <-
+  function(mapping = NULL, data = NULL, stat = "identity",
+           position = "identity", show.legend = NA, na.rm = FALSE,
+           inherit.aes = TRUE, xmin, xmax, ...) {
+    layer(geom = GeomTimeline, mapping = mapping, data = data, stat = stat,
+          position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+          params = list(na.rm = na.rm, xmin = xmin, xmax = xmax, ...))
+  }
+#####
+NOAAC %>%
+  dplyr::filter(COUNTRY %in% c("CHINA", "USA"),
+                between(lubridate::year(DATE),  2010, 2011)) %>%
+  ggplot(aes(x = DATE, y = COUNTRY, color = DEATHS, size = EQ_MAG_MS
+             )) +
+  geom_timeline(
+    xmin = eq_get_date(2010, 1, 1),
+    xmax = eq_get_date(2011, 12, 31))
+
+NOAAC %>%
+  dplyr::filter(COUNTRY %in% c("CHINA"),
+                between(lubridate::year(DATE), 2010, 2011)) %>%
+  ggplot(aes(x = DATE, y = COUNTRY, color = DEATHS
+             )) +
+  geom_timeline(
+    xmin = eq_get_date(2010, 1, 1),
+    xmax = eq_get_date(2011, 12, 31))
+
+NOAAC %>%
+  dplyr::filter(COUNTRY %in% c("CHINA", "USA", "IRAN"),
+                between(lubridate::year(DATE), 2010, 2011)) %>%
+  ggplot(aes(x = DATE, y = COUNTRY, color = DEATHS, size = EQ_MAG_MS)) +
+  geom_point(alpha = 0.5)
+#####direct####
 NOAAC %>%
   ggplot(aes(x = DATE, color = DEATHS, size = EQ_MAG_MS)) +
-  geom_point(alpha = 0.5, y = 0) +
-  xlim(eq_get_date(-2000, 1, 1), eq_get_date(0, 1, 1))
+  geom_point(alpha = 1, y = 0) +
+  xlim(eq_get_date(1988, 12, 1), eq_get_date(1988, 12, 31))
+
+NOAAC %>% filter(lubridate::year(DATE) == 1988) %>%
+  ggplot(aes(x = DATE, color = DEATHS, size = EQ_MAG_MS)) +
+  geom_segment(aes(x = min(DATE), xend = max(DATE), y = 0, yend = 0),
+               color = "green", alpha = 0.5, size = 1) +
+  geom_point(alpha = 0.5, y = 0)
