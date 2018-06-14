bd145c0c96eba1d4999e0e716286d4bd64668711
Valentin <konoshenkovalent@ukr.net>
2018-06-15 00:55:00 +0300

    Roxygen 4 Timeline

--- R/TimelineGeoms.R
+++ R/TimelineGeoms.R
@@ -1,4 +1,15 @@
-##### time line geom ####
+##### theme_timeline ####
+#' This theme is used to visualize earthquakes
+#'
+#' The theme is built on top \code{\link{theme_bw}} in order to follow the style
+#' of the Coursera's assignment
+#'
+#' @examples
+#' ggplot(mtcars) + geom_point(aes(x = wt, y = mpg)) + theme_timeline()
+#'
+#' @export
+#'
+#' @import ggplot2
 theme_timeline <- function() {
   ggplot2::theme_bw() +
     ggplot2::theme(
@@ -12,7 +23,7 @@ theme_timeline <- function() {
       axis.ticks.y = ggplot2::element_blank(),
       axis.line.x  = ggplot2::element_line(size = 1))
 }
-
+##### GeomTimeline ####
 GeomTimeline <- ggplot2::ggproto(
   "GeomTimeline", ggplot2::Geom, required_aes = "x",
   default_aes = ggplot2::aes(
@@ -44,7 +55,66 @@ GeomTimeline <- ggplot2::ggproto(
       timeline <- grid::gTree(children = grid::gList(eq_line, eq_point))}
   }
 )
+##### geom_timeline ####
+#' A time line of earthquakes
+#'
+#' This geom plots a time line of earthquakes.
+#' ranging from \code{xmin} to \code{xmax} dates with a point for each earthquake.
+#' Optional aesthetics include color, size, and alpha (for transparency).
+#' The \code{x} aesthetic is a date and an optional \code{y} aesthetic is a
+#' factor indicating some stratification in which case multiple time lines
+#' will be plotted for each level of the factor (e.g. country).
+#'
+#' @param xmin Min date for additional filtering
+#' @param xmax Max date for additional filtering
 
+#' @section Aesthetics:
+#'
+#' \code{geom_timeline} understands the following aesthetics
+#' (required aesthetics are in bold):
+#' \itemize{
+#'  \item \strong{x}
+#'  \item y
+#'  \item xmin
+#'  \item xmax
+#'  \item alpha
+#'  \item colour
+#'  \item fill
+#'  \item size
+#'  \item shape}
+#'
+#' @examples
+#' #without stratification
+#' file_read() %>%
+#'   eq_clean_data() %>%
+#'   dplyr::filter(COUNTRY %in% c("USA", "CHINA"),
+#'                 between(lubridate::year(DATE), 2010, 2011)) %>%
+#'   ggplot(aes(x = DATE,
+#'              color = DEATHS,
+#'              size = EQ_PRIMARY)) +
+#'   geom_timeline(alpha = 0.4) +
+#'   theme_timeline() +
+#'   labs(size = "Richter scale value", colour = "# deaths")
+#'
+#' #with stratification and additional filtering by time
+#' file_read() %>%
+#'   eq_clean_data() %>%
+#'   dplyr::filter(COUNTRY %in% c("USA", "CHINA", "CHILE"),
+#'                 between(lubridate::year(DATE), 2010, 2011)) %>%
+#'   ggplot(aes(x = DATE,
+#'              y = COUNTRY,
+#'              color = DEATHS,
+#'              size = EQ_PRIMARY)) +
+#'   geom_timeline(xmin = eq_get_date(2010, 1, 1),
+#'                 xmax = eq_get_date(2010, 12, 31),
+#                  alpha = 0.4) +
+#'   theme_timeline() +
+#'   labs(size = "Richter scale value", colour = "# deaths")
+#' @inheritParams ggplot2::geom_ribbon
+#'
+#' @export
+#'
+#' @import ggplot2
 geom_timeline <- function(mapping = NULL, data = NULL, stat = "identity",
                           position = "identity", show.legend = NA,
                           na.rm = FALSE, inherit.aes = TRUE,
@@ -53,8 +123,7 @@ geom_timeline <- function(mapping = NULL, data = NULL, stat = "identity",
         position = position, show.legend = show.legend, inherit.aes = inherit.aes,
         params = list(na.rm = na.rm, xmin = xmin, xmax = xmax, ...))
 }
-
-##### time line label geom ####
+##### GeomTimelineLabel ####
 GeomTimelineLabel <- ggplot2::ggproto(
   "GeomTimelineLabel", ggplot2::Geom,
   required_aes = c("x", "label"),
@@ -86,9 +155,49 @@ GeomTimelineLabel <- ggplot2::ggproto(
     grid::grobTree(lines, text)
   }
 )
-
+##### geom_timeline_label ####
+#' Annotations to the earthquake data
+#'
+#' This geom adds a vertical line to each data point with a text annotation
+#' (e.g. the location of the earthquake) attached to each line. There is an
+#' option to subset to \code{n_max} number of earthquakes, where we take the
+#' \code{n_max} largest (by magnitude) earthquakes.
+#' Aesthetics are \code{x}, which is the date of the earthquake and \code{label}
+#' which takes the column name from which annotations will be obtained.
+#' If \code{n_max} is omitted, each earthquake is annotated
+#'
+#' @param n_max A number of largest (by magnitude) earthquakes to be annotated.
+#' If omitted, each earthquake is annotated
+#'
+#' @section Aesthetics:
+#'
+#' \code{geom_timeline_label} understands the following aesthetics
+#' (required aesthetics are in bold):
+#' \itemize{
+#'  \item \strong{x}
+#'  \item \strong{label}
+#'  \item n_max}
+#'
+#' @examples
+#' #without stratification with 2 greatest earthquakes are annotated
+#' file_read() %>%
+#'   eq_clean_data() %>%
+#'   dplyr::filter(COUNTRY %in% c("USA", "CHINA"),
+#'                 between(lubridate::year(DATE), 2010, 2011)) %>%
+#'   ggplot(aes(x = DATE,
+#'              color = DEATHS,
+#'              size = EQ_PRIMARY)) +
+#'   geom_timeline(alpha = 0.4) +
+#'   theme_timeline() +
+#'   labs(size = "Richter scale value", colour = "# deaths") +
+#'   geom_timeline_label(aes(label = LOCATION_NAME), n_max = 2)
+#' @inheritParams ggplot2::geom_ribbon
+#'
+#' @export
+#'
+#' @import ggplot2
 geom_timeline_label <- function(mapping = NULL, data = NULL, stat = "identity",
-                                position = "identity", a.rm = FALSE,
+                                position = "identity", na.rm = FALSE,
                                 show.legend = NA, inherit.aes = TRUE, n_max = NA, ...) {
   ggplot2::layer(geom = GeomTimelineLabel, mapping = mapping, data = data,
                  stat = stat, position = position, show.legend = show.legend,
